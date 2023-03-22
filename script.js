f95c4d7b84cdde42d9b3ba965df7fb74c225bd65
Eric Sheffield <ersheff@gmail.com>
2023-03-22 14:51:13 -0400

    asdf

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-let yThreshold = document.getElementById("y-threshold");
-let yThresholdOutput = document.getElementById("y-threshold-output");
-let yValue = document.getElementById("y-value");
-
 let zThreshold = document.getElementById("z-threshold");
 let zThresholdOutput = document.getElementById("z-threshold-output");
 let zValue = document.getElementById("z-value");
 
 let accelPerm = document.getElementById("accel-perm");
 
-let yThresh = 0;
 let zThresh = 0;
-
-yThreshold.addEventListener("input", () => {
-  yThresh = yThreshold.value;
-  yThresholdOutput.innerText = yThresh;
-});
 
 zThreshold.addEventListener("input", () => {
   zThresh = zThreshold.value;
@@ -27,28 +17,15 @@ accelPerm.addEventListener("click", startAccel);
 function startAccel(){
   DeviceMotionEvent.requestPermission().then(response => {
     if (response === "granted") {
-      let lastY = 0;
-      let smoothY = 0;
+
       let lastZ = 0;
       let smoothZ = 0;
 
       window.addEventListener("devicemotion", (event) => {
-        let y = event.acceleration.y;
-        let z = event.acceleration.z;
-        
-        if (y > 0) {
-          smoothY = (lastY*0.85)+(y*0.15);
-          yValue.value = smoothY;
-          let yDiff = y - smoothY;
-          if (yDiff > yThresh) {
-            document.body.style.backgroundColor = "green";
-          }
-          else document.body.style.backgroundColor = "white";
-          lastY = smoothY;
-        }
+        let z = event.acceleration.z*10;
 
         if (z > 0) {
-          smoothZ = (lastZ*0.85)+(z*0.15);
+          smoothZ = (lastZ*0.75)+(z*0.25);
           zValue.value = smoothZ;
           let zDiff = z - smoothZ;
           if (zDiff > zThresh) {
