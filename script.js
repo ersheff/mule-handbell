e2b8cdfa85a8c4c54b74ceccacb535e981ec7685
Eric Sheffield <ersheff@gmail.com>
2023-03-22 14:08:55 -0400

    asdf

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let xyzOutput = document.getElementById("xyz-output");
-let abgOutput = document.getElementById("abg-output");
+let xOutput = document.getElementById("x-output");
+let yOutput = document.getElementById("y-output");
+let zOutput = document.getElementById("z-output");
 let accelPerm = document.getElementById("accel-perm");
 
 document.addEventListener("click", startAccel);
@@ -7,13 +8,10 @@ document.addEventListener("click", startAccel);
 function startAccel(){
   DeviceMotionEvent.requestPermission().then(response => {
     if (response === "granted") {
-      // xyz axes (units in m/s^2)
       window.addEventListener("devicemotion", (event) => {
-        xyzOutput.innerText = `x: ${event.acceleration.x}, y: ${event.acceleration.y}, z: ${event.acceleration.z}`;
-      });
-      // alpha-beta-gamma axes (units in degrees)
-      window.addEventListener("deviceorientation",(event) => {
-        abgOutput.innerText = `alpha: ${event.alpha}, beta: ${event.beta}, gamma: ${event.gamma}`;
+        xOutput.innerText = `x: ${event.acceleration.x}`;
+        yOutput.innerText = `y: ${event.acceleration.x}`;
+        zOutput.innerText = `z: ${event.acceleration.x}`;
       });
     }
   });
