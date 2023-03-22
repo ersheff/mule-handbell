608a4a14aa8b7ee7a4a538b2a66933aeebecff0d
Eric Sheffield <ersheff@gmail.com>
2023-03-22 14:47:38 -0400

    asdf

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-let zOutput = document.getElementById("z-output");
-let zDiffOutput = document.getElementById("z-diff-output");
+let yThreshold = document.getElementById("y-threshold");
+let yThresholdOutput = document.getElementById("y-threshold-output");
+let yValue = document.getElementById("y-value");
+
+let zThreshold = document.getElementById("z-threshold");
+let zThresholdOutput = document.getElementById("z-threshold-output");
+let zValue = document.getElementById("z-value");
+
 let accelPerm = document.getElementById("accel-perm");
 
-document.addEventListener("click", startAccel);
+let yThresh = 0;
+let zThresh = 0;
+
+yThreshold.addEventListener("input", () => {
+  yThresh = yThreshold.value;
+  yThresholdOutput.innerText = yThresh;
+});
+
+zThreshold.addEventListener("input", () => {
+  zThresh = zThreshold.value;
+  zThresholdOutput.innerText = zThresh;
+})
+
+accelPerm.addEventListener("click", startAccel);
+
 
 function startAccel(){
   DeviceMotionEvent.requestPermission().then(response => {
     if (response === "granted") {
+      let lastY = 0;
+      let smoothY = 0;
       let lastZ = 0;
       let smoothZ = 0;
-      let threshold = 5;
+
       window.addEventListener("devicemotion", (event) => {
+        let y = event.acceleration.y;
         let z = event.acceleration.z;
-        smoothZ = (lastZ*0.85)+(z*0.15);
-        zOutput.innerText = `z: ${smoothZ}`;
-        let zDiff = z - smoothZ;
-        zDiffOutput.innerText = `zDiff: ${zDiff}`;
-        if (zDiff > threshold) {
-          document.body.style.backgroundColor = "red";
+        
+        if (y > 0) {
+          smoothY = (lastY*0.85)+(y*0.15);
+          yValue.value = smoothY;
+          let yDiff = y - smoothY;
+          if (yDiff > yThresh) {
+            document.body.style.backgroundColor = "green";
+          }
+          else document.body.style.backgroundColor = "white";
+          lastY = smoothY;
         }
-        else document.body.style.backgroundColor = "white";
-        lastZ = smoothZ;
+
+        if (z > 0) {
+          smoothZ = (lastZ*0.85)+(z*0.15);
+          zValue.value = smoothZ;
+          let zDiff = z - smoothZ;
+          if (zDiff > zThresh) {
+            document.body.style.backgroundColor = "red";
+          }
+          else document.body.style.backgroundColor = "white";
+          lastZ = smoothZ;
+        }
+
       });
     }
   });
