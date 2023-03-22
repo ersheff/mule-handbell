ac8dfdd87c1950a478641078b3fc117f12ff2cf7
Eric Sheffield <ersheff@gmail.com>
2023-03-22 14:29:07 -0400

    asdf

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let zOutput = document.getElementById("z-output");
 let accelPerm = document.getElementById("accel-perm");
 
 document.addEventListener("click", startAccel);
@@ -6,13 +7,17 @@ function startAccel(){
   DeviceMotionEvent.requestPermission().then(response => {
     if (response === "granted") {
       let lastZ = 0;
+      let smoothZ = 0;
       let threshold = 50;
       window.addEventListener("devicemotion", (event) => {
         let z = event.acceleration.z;
-        if (z - lastZ > threshold) {
+        smoothZ = (lastZ*0.85)+(z*0.15);
+        zOutput.innerText = `z: ${smoothZ}`
+        if (z - smoothZ > threshold) {
           document.body.style.backgroundColor = "red";
         }
         else document.body.style.backgroundColor = "white";
+        lastZ = smoothZ;
       });
     }
   });
