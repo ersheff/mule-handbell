8f46d659d6e6750885a5c4c38a0ec5c8598b80fb
Eric Sheffield <ersheff@gmail.com>
2023-03-22 14:24:56 -0400

    asdf

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let xOutput = document.getElementById("x-output");
-let yOutput = document.getElementById("y-output");
-let zOutput = document.getElementById("z-output");
 let accelPerm = document.getElementById("accel-perm");
 
 document.addEventListener("click", startAccel);
