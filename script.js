4479beff69f651b9159824f59f7a51bd1acd97c2
Eric Sheffield <ersheff@gmail.com>
2023-03-27 14:38:47 -0400

    asdf

--- script.js
+++ script.js
@@ -2,21 +2,56 @@ let zThreshold = document.getElementById("z-threshold");
 let zThresholdOutput = document.getElementById("z-threshold-output");
 let zValue = document.getElementById("z-value");
 
-let startButton = document.getElementById("start-button");
+let startAccel = document.getElementById("start-accel");
+let startSound = document.getElementById("start-sound");
 
 let zThresh = 0;
 let debounceTimer = 0;
+let polySynth;
 
 zThreshold.addEventListener("input", () => {
   zThresh = zThreshold.value;
   zThresholdOutput.innerText = zThresh;
 })
 
-startButton.addEventListener("click", async () => {
+startAccel.addEventListener("click", getAccel);
+
+function getAccel() {
+
+  DeviceMotionEvent.requestPermission().then(response => {
+    if (response === "granted") {
+
+      window.addEventListener("devicemotion", (event) => {
+        let z = event.acceleration.z*10;
+
+        if (z > 0) {
+          smoothZ = (lastZ*0.75)+(z*0.25);
+          zValue.value = smoothZ;
+          let zDiff = z - smoothZ;
+          if (zDiff > zThresh && debounceTimer <= 0) {
+            document.body.style.backgroundColor = "red";
+            polySynth.triggerAttackRelease(60, 1, Tone.immediate(), 1);
+            debounceTimer = 30;
+          }
+          else document.body.style.backgroundColor = "white";
+          lastZ = smoothZ;
+        }
+
+        if (debounceTimer-- <= 0) {
+          debounceTimer = 0;
+        }
+
+      });
+    }
+  });
+}
+
+
+startSound.addEventListener("click", async () => {
   
   await Tone.start();
 
-  const polySynth = new Tone.PolySynth({
+  polySynth = new Tone.PolySynth({
     voice: Tone.MonoSynth
   });
 
@@ -45,35 +80,5 @@ startButton.addEventListener("click", async () => {
   });
 
   polySynth.toDestination();
-
-  DeviceMotionEvent.requestPermission().then(response => {
-    if (response === "granted") {
-
-      let lastZ = 0;
-      let smoothZ = 0;
-
-      window.addEventListener("devicemotion", (event) => {
-        let z = event.acceleration.z*10;
-
-        if (z > 0) {
-          smoothZ = (lastZ*0.75)+(z*0.25);
-          zValue.value = smoothZ;
-          let zDiff = z - smoothZ;
-          if (zDiff > zThresh && debounceTimer <= 0) {
-            document.body.style.backgroundColor = "red";
-            polySynth.triggerAttackRelease(60, 1, Tone.immediate(), 1);
-            debounceTimer = 30;
-          }
-          else document.body.style.backgroundColor = "white";
-          lastZ = smoothZ;
-        }
-
-        if (debounceTimer-- <= 0) {
-          debounceTimer = 0;
-        }
-
-      });
-    }
-  });
 
 });
